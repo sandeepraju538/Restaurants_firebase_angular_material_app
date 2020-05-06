7630a60105fa6ecb88a7e7a83e8f63dcb3d2446a
sandeepraju538 <sandeepraju538@gmail.com>
2020-05-07 01:51:04 +0530

    changes made for git deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBTBMBIVK7hvWpAewxvKJiN_dUp579Xwuc',
+    authDomain: 'restaurants-data-854c0.firebaseapp.com',
+    databaseURL: 'https://restaurants-data-854c0.firebaseio.com',
+    projectId: 'restaurants-data-854c0',
+    storageBucket: 'restaurants-data-854c0.appspot.com',
+    messagingSenderId: '958312550817',
+    appId: '1:958312550817:web:627495a6f79c15f03c3be0'
+  }
 };
